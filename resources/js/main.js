da20755575ed1dd14dcbea196ecc4dea0af010d3
Shrawan Kumar <shrawan8565@gmail.com>
2021-07-04 19:01:01 +0530

    update

--- resources/js/main.js
+++ resources/js/main.js
@@ -11,8 +11,8 @@ $(".carousel").owlCarousel({
         0:{
             items:1
         },
-        600:{
-            items:1
+        700:{
+            items:2
         },
         1000:{
             items:4
