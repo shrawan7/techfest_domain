300101ee59a84abbba688f3fb4053c323ef6b762
Shrawan Kumar <shrawan8565@gmail.com>
2021-07-04 11:45:05 +0530

    initial commit

---
+++ resources/js/main.js
@@ -0,0 +1,21 @@
+$(".carousel").owlCarousel({
+    margin: 20,
+    loop: false,
+    autoplay: false,
+    dots: false,
+    nav: true, 
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    items: 5,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
